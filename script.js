1aa071e70cb7e9c97ef554fd11ba07e2bf86d626
AnonymousXbkl <kerjaandi01@gmail.com>
2024-02-17 17:23:22 +0700

    menambahkan menu products

--- script.js
+++ script.js
@@ -4,10 +4,31 @@ const navbarNav = document.querySelector ('.navbar-nav');
         navbarNav.classList.toggle('active');
     };
 
+
+
 // Click di luar sidebar untuk hilangkan Nav
 const hamburger = document.querySelector ('#hamburger-menu');
+const sb = document.querySelector ('#search-box');
     document.addEventListener ('click', function (e) {
         if (!hamburger.contains(e.target) && !navbarNav.contains (e.target)) {
             navbarNav.classList.remove ('active');
         };
+        if (!sb.contains(e.target) && !searchForm.contains (e.target) && !searchBtn.contains (e.target)) {
+            searchForm.classList.remove ('active');
+        };
+        e.preventDefault ();
+    });
+
+
+
+// Click Tombol Search di Navbar
+const searchForm = document.querySelector ('.search-form');
+const searchBox = document.querySelector ('#search-box');
+const searchBtn = document.querySelector ('#search-button')
+    searchBtn.addEventListener ('click', function (e) {
+
+        searchForm.classList.toggle ('active');
+        searchBox.focus ();
+        e.preventDefault ();
+    
     });
