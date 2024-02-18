9823bc9210432832c8ae7a99837f72b60ae80406
AnonymousXbkl <kerjaandi01@gmail.com>
2024-02-18 11:11:22 +0700

    menambahkan CSS Masking

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ const navbarNav = document.querySelector ('.navbar-nav');
         navbarNav.classList.toggle('active');
     };
 
-
-
 // Click di luar sidebar untuk hilangkan Nav
 const hamburger = document.querySelector ('#hamburger-menu');
 const sb = document.querySelector ('#search-box');
@@ -16,10 +14,7 @@ const sb = document.querySelector ('#search-box');
         if (!sb.contains(e.target) && !searchForm.contains (e.target) && !searchBtn.contains (e.target)) {
             searchForm.classList.remove ('active');
         };
-        e.preventDefault ();
     });
-
-
 
 // Click Tombol Search di Navbar
 const searchForm = document.querySelector ('.search-form');
@@ -30,5 +25,40 @@ const searchBtn = document.querySelector ('#search-button')
         searchForm.classList.toggle ('active');
         searchBox.focus ();
         e.preventDefault ();
-    
     });
+
+// Shopping Cart
+const shoppingCartButton = document.querySelector ('#shopping-cart-button')
+const shoppingCart = document.querySelector ('.shopping-cart')
+    document.addEventListener('click', function(e) {
+        if (!shoppingCartButton.contains(e.target) && !shoppingCart.contains(e.target)) {
+            shoppingCart.classList.remove ('active')
+        }
+    });
+        shoppingCartButton.addEventListener ('click', function (e) {
+            shoppingCart.classList.toggle ('active');
+        
+    });
+
+// Item Detail Modal
+const itemDetailModal = document.querySelector ('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll ('.item-detail-button');
+    for (i = 0; i < itemDetailButtons.length; i++) {
+        itemDetailButtons[i].addEventListener ('click', function (e) {
+            itemDetailModal.style.display = 'flex'
+            e.preventDefault ();
+        })
+    }
+    window.addEventListener ('click', function (e) {
+        if (e.target === itemDetailModal) {
+            itemDetailModal.style.display = 'none';
+        };
+    });
+
+
+// Close Item Detail Modal
+const closeItemDetail = document.querySelector ('.modal .close-icon');
+    closeItemDetail.addEventListener ('click', function (e) {
+        itemDetailModal.style.display = 'none';
+        e.preventDefault ();
+    })
